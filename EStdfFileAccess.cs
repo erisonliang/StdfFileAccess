dc47923eb919d9c8aeb1082375a41427e4d59f4a
E <t>
2019-04-26 17:44:28 +0900

    up

---
+++ EStdfFileAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace EStdfFileAccess
+{
+    public class EStdfHeader
+    {
+        public const Byte REC_TYPE_0 = 0;
+        public const Byte REC_TYPE_1 = 1;
+
+    }
+    public class EStdfFile
+    {
+    }
+}
